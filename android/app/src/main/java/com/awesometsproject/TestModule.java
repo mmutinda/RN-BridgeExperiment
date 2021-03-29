0792bb87b74e420ffff9c761f65fbe535ebac0dd
Michael Mutinda <mmutinda@microsoft.com>
2021-03-29 20:51:24 +0300

    added test method

--- android/app/src/main/java/com/awesometsproject/TestModule.java
+++ android/app/src/main/java/com/awesometsproject/TestModule.java
@@ -1,11 +1,17 @@
 package com.awesometsproject;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
 
 public class TestModule extends ReactContextBaseJavaModule {
+
+    private static final String TAG = "TestModule";
+
     TestModule(ReactApplicationContext context) {
         super(context);
     }
@@ -14,5 +20,10 @@ public class TestModule extends ReactContextBaseJavaModule {
     @Override
     public String getName() {
         return "TestModule";
+    }
+
+    @ReactMethod
+    public void testMethod() {
+        Log.d(TAG, "testMethod: this is called from js side: michael");
     }
 }
